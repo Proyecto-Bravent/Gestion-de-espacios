410d5260afe23ec46eebaeb6ecdebaa4d1813abc
jorgeperezdev <jorgeplanuza@gmail.com>
2022-06-25 13:23:40 +0200

    comments

--- Frontend/gestion-de-espacios/src/app/directives/admin.directive.ts
+++ Frontend/gestion-de-espacios/src/app/directives/admin.directive.ts
@@ -9,6 +9,8 @@ export class AdminDirective {
 
   constructor(element: ElementRef) {
 
+    // Get the token from the localStorage and decode it to get the user data (token). Then, check if the user is an admin. If so, show the element.
+
     const payload = jwt_decode(localStorage.getItem('token')!) as any;
     if (!payload.is_admin) {
       element.nativeElement.style.display = 'none'
